c84a7b09631ac38ff2424a8fb5fdd5a8a75a89de
PhanQuang <phanquang1830@gmail.com>
2025-05-08 21:48:35 +0700

    First commit: Hello world server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) =>{
-    res.send('Hello world');
+    res.send('Hello world ');
 })
 
 app.listen(PORT, () =>{
