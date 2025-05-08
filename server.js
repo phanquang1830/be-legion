d16494f582c5d9f4222d1784d8683700e84f3dc2
PhanQuang <phanquang1830@gmail.com>
2025-05-08 21:33:33 +0700

    Test commit on feature branch

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) =>{
-    res.send('Hello world 123123');
+    res.send('Hello world');
 })
 
 app.listen(PORT, () =>{
