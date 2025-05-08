b718e3144c40e7f4f6c2cdafedcd536b9da2062b
MinhQuang-dev <phanquang704@gmail.com>
2025-05-08 15:38:22 +0700

    First commit: Hello world server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT
+
+app.get('/', (req, res) =>{
+    res.send('Hello world');
+})
+
+app.listen(PORT, () =>{
+    console.log(`App listen on port ${PORT}`)
+})
+
