1a345e3671e481942e3d969ccfe8dd0b641d31eb
PhanQuang <phanquang1830@gmail.com>
2025-05-26 23:09:17 +0700

    feat: add routes and middleware to server.js

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express from 'express'
+import dotenv from 'dotenv'
+
+import connectDB from './config/db.js';
+import eventRouter from './routes/event.routes.js'
+import { notFound, errorHandler } from './middlewares/error.middleware.js';
 
 dotenv.config();
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT
 
-app.get('/', (req, res) =>{
-    res.send('Hello world');
-})
+app.use('/api/event', eventRouter)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, () =>{
     console.log(`App listen on port ${PORT}`)
