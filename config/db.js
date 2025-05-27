1e9195e7422eb99865eb5db1de3a5193e6bc3b99
LeTonVinh <le.tonvinh1@gmail.com>
2025-05-27 23:20:28 +0700

    update connect db

--- config/db.js
+++ config/db.js
@@ -1,23 +1,18 @@
-// db.js
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config(); // Load environment variables from .env file
+dotenv.config();
 
+const dbURI = process.env.MONGODB_URI;
 
-
-const dbURI = process.env.MONGODB_URI // Ensure you have set this in your .env file
 const connectDB = async () => {
     try {
-        await mongoose.connect(dbURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(dbURI); // Không cần các tùy chọn cũ
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection error:', error);
-        process.exit(1); // Exit the process with failure
+        process.exit(1);
     }
 };
-export default connectDB;
 
+export default connectDB;
