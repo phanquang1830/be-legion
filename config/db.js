22ebbbc2c649aaaea84141bb7c23be76489dc606
LeTonVinh <le.tonvinh1@gmail.com>
2025-05-27 22:58:48 +0700

    connect db  with mongodb

---
+++ config/db.js
@@ -0,0 +1,21 @@
+// db.js
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const dbURI = process.env.MONGODB_URI // Ensure you have set this in your .env file
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dbURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Exit the process with failure
+    }
+};
+export default connectDB;
+
