cfb6659f0a136d418826f4498fc3361ab5acb906
LeTonVinh <le.tonvinh1@gmail.com>
2025-05-28 23:41:34 +0700

    dua swagger truy cap tren web

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js'; // Adjust the path as necessary
 import eventRoutes from './routes/Event.routes.js'; // Adjust the path as necessary
 import errorHandler from './middlewares/errorHandler.js';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 dotenv.config();
 connectDB(); // Connect to MongoDB
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const swaggerDocument = YAML.load('./event.api.swagger.yaml');
 
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/events', eventRoutes); // Use the event routes
 app.use(errorHandler); // Use the error handler middleware
 
