77be1c3da052849f2dec3a5fd6232d46064a9059
LeTonVinh <le.tonvinh1@gmail.com>
2025-05-28 18:26:52 +0700

    mupdate model

--- models/Event.model.js
+++ models/Event.model.js
@@ -1,33 +1,93 @@
 import mongoose from 'mongoose';
+
 const eventSchema = new mongoose.Schema({
   id: {
     type: String,
     required: true,
-    unique: true, // đảm bảo id là duy nhất
+    unique: true,
+    description: 'ID duy nhất của Event, tự động tăng'
   },
-  title: {
+  name: {
     type: String,
     required: true,
+    description: 'Tên Event'
   },
-  description: {
+  slug: {
     type: String,
+    description: 'URL thân thiện của Event'
   },
-  startDate: {
+  host: {
+    type: String,
+    description: 'Người tổ chức Event'
+  },
+  community_member_id: {
+    type: String,
+    description: 'ID của thành viên tổ chức'
+  },
+  in_person_location: {
+    type: String,
+    description: 'Địa điểm tổ chức Event'
+  },
+  starts_at: {
     type: Date,
     required: true,
+    description: 'Thời gian Event bắt đầu'
   },
-  endDate: {
+  ends_at: {
     type: Date,
     required: true,
+    description: 'Thời gian Event kết thúc'
   },
-  location: {
+  created_at: {
+    type: Date,
+    default: Date.now,
+    description: 'Thời gian tạo Event'
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+    description: 'Thời gian cập nhật Event gần nhất'
+  },
+  url: {
     type: String,
+    description: 'URL đầy đủ của Event'
   },
   cover_image_url: {
     type: String,
+    description: 'URL ảnh bìa của Event'
   },
-}, { 
-  collection: 'events' // tên collection trong MongoDB
+  description: {
+    type: String,
+    description: 'Mô tả chi tiết Event'
+  },
+  price: {
+    type: Number,
+    description: 'Giá vé hoặc phí tham dự (Đơn vị VND)'
+  },
+  max_attendees: {
+    type: Number,
+    description: 'Số lượng người tham gia tối đa'
+  },
+  current_attendees: {
+    type: Number,
+    description: 'Số lượng người đăng kí'
+  },
+  status: {
+    type: String,
+    enum: ['upcoming', 'ongoing', 'finished', 'cancelled'],
+    description: 'Trạng thái (upcoming/ongoing/finished/cancelled)'
+  },
+  category: {
+    type: String,
+    description: 'Loại Event (workshop/talk/networking/yoga...)'
+  },
+  location_URL: {
+    type: String,
+    description: 'URL Google Map của Event'
+  }
+}, {
+  collection: 'events'
 });
+
 const Event = mongoose.model('Event', eventSchema);
 export default Event;
