6ecf806fbb886da2b706ca5a80a2d06af76e4e70
PhanQuang <phanquang1830@gmail.com>
2025-05-29 17:46:33 +0700

    feat: integrate paginate middleware into eventController

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -1,12 +1,13 @@
 import Event from '../models/event.model.js'
 import asyncHandler from 'express-async-handler';
+import paginateWithSearch from '../middlewares/paginate.middleware.js';
 
 // @desc Fetch Event
 // @route GET /api/event
 // @access Public
 const getAllListEvent = asyncHandler (async (req, res) =>{
     const event = await Event.find();
-    res.json({Event: event});
+    res.json(res.paginatedResult);
 });
 
 // @desc Fetch single Event 
