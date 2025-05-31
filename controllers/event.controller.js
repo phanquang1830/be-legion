76f72443f85f4075066f7589834ee88836cb4ff7
PhanQuang <phanquang1830@gmail.com>
2025-05-31 20:35:44 +0700

    refactor(controller): rewrite Event controller from Mongo to MySql with Sequelizez

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -5,47 +5,57 @@ import asyncHandler from 'express-async-handler';
 // @route GET /api/event
 // @access Public
 const getAllListEvent = asyncHandler (async (req, res) =>{
-    res.json(res.paginatedResult);
+    res.json({
+        statusCode: 200,
+        message: 'Get Event success',
+        ...res.paginatedResult,
+    });
 });
 
 // @desc Fetch single Event 
 // @route GET /api/event/:id
 // @access Public
 const getEventById = asyncHandler (async (req, res) =>{
-    const event = await Event.findById(req.params.id);
+    const event = await Event.findByPk(req.params.id);
 
     if(!event) {
         res.status(404)
         throw new Error("Event not Found")
     }
-    return res.json(event);
+    return res.json({
+        statusCode: 200,
+        message: 'Get Event By Id Success',
+        data: event
+    });
 });
 
-const createEvent = asyncHandler (async (req, res) =>{
-    const event = req.body;
-    console.log(req.body)
+const createEvent = asyncHandler(async (req, res) => {
+  const event = req.body
+  console.log(req.body)
 
-    const createEvent = await new Event ({
-        name: event.name,
-        slug: event.slug,
-        host: event.host,
-        community_member_id: event.community_member_id,
-        in_person_location: event.in_person_location,
-        starts_at: event.starts_at,
-        ends_at: event.ends_at,
-        create_at: event.create_at,
-        updated_at: event.updated_at,
-        url: event.url,
-        cover_image_url: event.cover_image_url,
-        description: event.description,
-        price: event.price,
-        max_attendees: event.max_attendees,
-        current_attendees: event.current_attendees,
-        status: event.status,
-        category: event.category,
-        location_URL: event.location_URL
-    }).save();
-    res.status(201).json(createEvent)
+  // Sequelize dùng create() thay vì new Event().save()
+  const createdEvent = await Event.create({
+    name: event.name,
+    slug: event.slug,
+    host: event.host,
+    community_member_id: event.community_member_id,
+    in_person_location: event.in_person_location,
+    starts_at: event.starts_at,
+    ends_at: event.ends_at,
+    created_at: event.created_at, 
+    updated_at: event.updated_at,
+    url: event.url,
+    cover_image_url: event.cover_image_url,
+    description: event.description,
+    price: event.price,
+    max_attendees: event.max_attendees,
+    current_attendees: event.current_attendees,
+    status: event.status,
+    category: event.category,
+    location_URL: event.location_URL,
+  })
+
+  res.status(201).json(createdEvent)
 })
 
 export {
