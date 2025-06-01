9dc0ec14a38dd36f7c83f94233cd2ead8d2c2a93
PhanQuang <phanquang1830@gmail.com>
2025-06-01 16:25:14 +0700

    Chore: remove unused createEvent API

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -50,38 +50,7 @@ const getEventById = asyncHandler(async (req, res) => {
   });
 });
 
-
-const createEvent = asyncHandler(async (req, res) => {
-  const event = req.body
-  console.log(req.body)
-
-  // Sequelize dùng create() thay vì new Event().save()
-  const createdEvent = await Event.create({
-    name: event.name,
-    slug: event.slug,
-    host: event.host,
-    community_member_id: event.community_member_id,
-    in_person_location: event.in_person_location,
-    starts_at: event.starts_at,
-    ends_at: event.ends_at,
-    created_at: event.created_at, 
-    updated_at: event.updated_at,
-    url: event.url,
-    cover_image_url: event.cover_image_url,
-    description: event.description,
-    price: event.price,
-    max_attendees: event.max_attendees,
-    current_attendees: event.current_attendees,
-    status: event.status,
-    category: event.category,
-    location_URL: event.location_URL,
-  })
-
-  res.status(201).json(createdEvent)
-})
-
 export {
     getAllListEvent,
     getEventById,
-    createEvent
 };
