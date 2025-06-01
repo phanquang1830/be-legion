25a036f42583c29c1ed6cbdc382a23bb8b6b6825
PhanQuang <phanquang1830@gmail.com>
2025-06-01 16:25:43 +0700

    Chore: remove unused createEvent API

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -3,13 +3,12 @@ const router = express.Router();
 import {
     getAllListEvent,
     getEventById,
-    createEvent
 } from '../controllers/event.controller.js'
 
 router.route('/')
     .get(getAllListEvent)
-    .post(createEvent)
-
-router.route('/:id').get(getEventById)
+    
+router.route('/:id')
+    .get(getEventById)
 
 export default router
