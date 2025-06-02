fc3318d0954ee2ef2ba017c4a69d18ee094dd2c4
PhanQuang <phanquang1830@gmail.com>
2025-06-02 15:50:21 +0700

    Fix: min pool default = 1

--- config/db.js
+++ config/db.js
@@ -20,7 +20,7 @@ const sequelize = new Sequelize(
 
     pool: {
       max: parseInt(process.env.DB_POOL_MAX) || '10', // Số kết nối tối đa
-      min: parseInt(process.env.DB_POOL_MIN) || '0', // Số kết nối tối thiểu
+      min: parseInt(process.env.DB_POOL_MIN) || '1', // Số kết nối tối thiểu
       acquire: parseInt(process.env.DB_POOL_ACQUIRE) || '30000', // Thời gian tối đa (ms) để sequelize cố gắng 
                                                                 // lấy 1 kết nối trước khi báo lỗi
       idle: parseInt(process.env.DB_POOL_IDLE) || '10000' // Thời gian tối đa một kết nối rảnh hoạt động trước khi bị đóng (ms)
