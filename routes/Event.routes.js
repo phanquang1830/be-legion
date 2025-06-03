02c62e27a05c709547919c5fc07d5acfe489c467
LeTonVinh <le.tonvinh1@gmail.com>
2025-06-03 23:26:41 +0700

    update event routout de phu hop voi phan trang moimoi

--- routes/Event.routes.js
+++ routes/Event.routes.js
@@ -1,12 +1,14 @@
 // routes/event.routes.js
 import express from "express";
 import { getAllEventController, getEventByIdController } from "../controllers/Event.controller.js";
+import paginate from "../middlewares/paginate.js";
+import Event from "../models/Event.model.js";
 
 const router = express.Router();
 
 // Lấy danh sách tất cả sự kiện
 router.route("/")
-.get(getAllEventController);
+.get(paginate(Event), getAllEventController);
 
 // Lấy thông tin sự kiện theo ID (nên dùng query hoặc route param cho chuẩn REST)
 router.route("/:id")
@@ -14,4 +16,4 @@ router.route("/:id")
 
 // Export hàm để khởi tạo route vào app
 
-export default (router);
+export default router;
